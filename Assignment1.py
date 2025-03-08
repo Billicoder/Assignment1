fd77852f48a54c1c8c39b3011899e3b049565417
Billicoder <110827066+Billicoder@users.noreply.github.com>
2025-03-08 18:17:13 +0530

    Add files via upload
    I have completed Assignment 1.

---
+++ Assignment1.py
@@ -0,0 +1,55 @@
+{
+  "metadata": {
+    "kernelspec": {
+      "name": "python",
+      "display_name": "Python (Pyodide)",
+      "language": "python"
+    },
+    "language_info": {
+      "codemirror_mode": {
+        "name": "python",
+        "version": 3
+      },
+      "file_extension": ".py",
+      "mimetype": "text/x-python",
+      "name": "python",
+      "nbconvert_exporter": "python",
+      "pygments_lexer": "ipython3",
+      "version": "3.8"
+    }
+  },
+  "nbformat_minor": 5,
+  "nbformat": 4,
+  "cells": [
+    {
+      "id": "3f6b6bf6-799f-40c9-aad0-3a6f515f6911",
+      "cell_type": "code",
+      "source": "#5 variable declaration\n#Bank balance\nAman=10000\nRohit=39000\nPabkaj=5000\nKunal=4\nKiran=2500\n\n",
+      "metadata": {
+        "trusted": true
+      },
+      "outputs": [],
+      "execution_count": 1
+    },
+    {
+      "id": "378df4c1-36c3-45fa-a629-82ae69f50af6",
+      "cell_type": "code",
+      "source": "",
+      "metadata": {
+        "trusted": true
+      },
+      "outputs": [],
+      "execution_count": null
+    },
+    {
+      "id": "c365ece9-a361-4397-9827-c8423b632f0c",
+      "cell_type": "code",
+      "source": "",
+      "metadata": {
+        "trusted": true
+      },
+      "outputs": [],
+      "execution_count": null
+    }
+  ]
+}
